75905c15ae336b36147b25e1ef769e9e5493a8d2
minujeong <minu.hanwool@gmail.com>
2018-03-10 05:22:23 +0800

    adjust colors/params

--- static/js/app.js
+++ static/js/app.js
@@ -65,55 +65,6 @@ function initThree()
     camera.rotation.x = -Math.atan2(camera.position.y, camera.position.z);
     scene.add(camera);
 
-    entity = new THREE.Object3D();
-    scene.add(entity);
-
-    let rotator = new THREE.Object3D();
-    rotator.position.x = 10;
-    rotator.position.y = 2;
-    rotator.position.z = 2;
-    rotator.rotation.y = -Math.PI * 0.15;
-    anime({
-        targets: rotator.rotation,
-        y: -Math.PI * 2.15,
-        duration: 2000,
-        loop: true,
-    });
-    scene.add(rotator);
-
-    let fontloader = new THREE.FontLoader();
-    fontloader.load("static/fonts/Ubuntu_Bold.json", function(font)
-    {
-        let textGeom = new THREE.TextGeometry(
-            "anime js", {
-                size: 2, height: 1, curveSegments: 5,
-                font: font,
-            });
-
-        let material = new THREE.MeshStandardMaterial({ color: 0xffdd77 });
-        let mesh = new THREE.Mesh(textGeom, material);
-        mesh.position.x = - Math.max(...textGeom.vertices.map((v)=>v.x)) * 0.5;
-        rotator.add(mesh);
-    });
-
-    light = new THREE.PointLight(0xffffff, 2.0);
-    light.castShadow = true;
-    light.position.x = 5;
-    light.position.y = 5;
-    light.position.z = 10;
-    anime({
-        targets: light.position,
-        x: -5,
-        loop: true,
-        easing: 'linear',
-        direction: 'alternate',
-        duration: 2000,
-    });
-    scene.add(light);
-
-    let ambientLight = new THREE.AmbientLight(0x403080);
-    scene.add(ambientLight);
-
     renderer.setSize(W, H);
     stage.appendChild(renderer.domElement);
 }
@@ -133,6 +84,95 @@ function initAmmo()
 
 function initScene()
 {
+    // anime js text
+    let rotator = new THREE.Object3D();
+    rotator.position.x = 10;
+    rotator.position.y = 5;
+    rotator.position.z = 4;
+    rotator.rotation.y = Math.PI * 3.85;
+    scene.add(rotator);
+    let fontloader = new THREE.FontLoader();
+    fontloader.load("static/fonts/Ubuntu_Bold.json", function(font)
+    {
+        let textGeom = new THREE.TextGeometry(
+            "anime js", {
+                size: 2, height: 0.35, curveSegments: 12,
+                font: font,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.01,
+            });
+
+        let material = new THREE.MeshStandardMaterial({ color: 0xffdd77 });
+        let mesh = new THREE.Mesh(textGeom, material);
+        let width = Math.max(...textGeom.vertices.map((v)=>v.x));
+        mesh.position.x = - width * 0.5;
+        rotator.add(mesh);
+
+        let circShadowGeom = new THREE.CircleGeometry(2.5, 24);
+        let circShadowMesh = new THREE.Mesh(
+            circShadowGeom, new THREE.MeshBasicMaterial({ color: 0x191919 }))
+        circShadowMesh.position.copy(rotator.position);
+        circShadowMesh.position.y = 0;
+        circShadowMesh.rotation.x = - Math.PI * 0.5;
+        scene.add(circShadowMesh);
+
+        // instead of simple loop, this reevaluates randoms
+        function rotate()
+        {
+            anime.remove(rotator.rotation);
+            rotator.rotation.y %= Math.PI * 2;
+            anime({
+                targets: rotator.rotation,
+                y: Math.PI * 3.85 + Math.random() * 0.1 - 0.05,
+                duration: 2000,
+                complete: ()=>rotate()
+            });
+        }
+        rotate();
+        anime({
+            targets: rotator.position,
+            y: 2.5,
+            direction: "alternate",
+            easing: "linear",
+            duration: 2000,
+            loop: true,
+        });
+
+        anime({
+            targets: circShadowMesh.scale,
+            x: 1.25,
+            y: 1.25,
+            z: 1.25,
+            direction: "alternate",
+            easing: "linear",
+            duration: 2000,
+            loop: true,
+        });
+    });
+
+    // light
+    light = new THREE.PointLight(0xffffff, 1.0);
+    light.castShadow = true;
+    light.position.x = 5;
+    light.position.y = 5;
+    light.position.z = 10;
+    function blink()
+    {
+        anime({
+            targets: light,
+            intensity: 0.8 + Math.random() * 0.1,
+            direction: 'alternate',
+            duration: 1000 + Math.random() * 1000,
+            complete: blink,
+        });
+    }
+    blink();
+    scene.add(light);
+
+    let ambientLight = new THREE.AmbientLight(0x403080);
+    scene.add(ambientLight);
+
     // floor
     addBody(new THREE.Object3D(),
         new Ammo.btBoxShape(new Ammo.btVector3(20, 1, 20)),
@@ -140,12 +180,8 @@ function initScene()
         quat=null,
         mass=0,
     );
-
-    let boxGeom = new THREE.BoxGeometry(
-        width=1, height=1, depth=1,
-        widthSegments=2, heightSegments=2, lengthSegments=2,
-    );
     
+    // ammo
     let rotation = new THREE.Quaternion();
     for (var x = 0; x < 4; x++)
     {
@@ -153,24 +189,41 @@ function initScene()
         {
             for (var z = 0; z < 4; z++)
             {
-                let r = (x / 4) * 0xff << 16;
-                let g = (y / 4) * 0xff << 8;
-                let b = (z / 4) * 0xff << 0;
-                let v = Math.pow(Math.random(), 4.0);
+                let geom = null;
+                let shape = null;
+                if (Math.random() < 0.9)
+                {
+                    geom = new THREE.BoxGeometry(1, 1, 1);
+                    shape = new Ammo.btBoxShape(new Ammo.btVector3(.5, .5, .5));
+                } else
+                {
+                    geom = new THREE.SphereGeometry(
+                        radius=0.5, widthSegments=6, heightSegments=6
+                    );
+                    shape = new Ammo.btSphereShape(0.25);
+                }
+
+                let v = Math.pow(Math.random(), 2.0);
                 let boxMat = new THREE.MeshStandardMaterial({
-                    color: r | g | b,
+                    color: (x / 4) * 0xff << 16 | (y / 4) * 0xff << 8 | (z / 4) * 0xff << 0,
                     smoothness: v,
                     matalness: v,
+                    wireframe: true,
+                    wireframeLinewidth: 0.25,
                 });
+
                 rotation.setFromAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI * (Math.random() * 0.5));
-                addBody(new THREE.Mesh(boxGeom, boxMat),
-                    new Ammo.btBoxShape(new Ammo.btVector3(.5, .5, .5)),
+                let mesh = new THREE.Mesh(geom, boxMat);
+                addBody(
+                    mesh,
+                    shape,
                     pos=new THREE.Vector3(
-                        -10 + x + Math.random() * 0.2 - 0.1,
+                        -10 + x + Math.random() * 0.4 - 0.2,
                         5 + y + Math.random() * 0.2 - 0.1,
-                        2.5 + z + Math.random() * 0.2 - 0.1),
+                        3 + z + Math.random() * 0.4 - 0.2),
                     quat=rotation
                 );
+                scene.add(mesh);
             }
         }
     }
@@ -225,7 +278,6 @@ function addBody(threeObj, ammoShape, pos=null, quat=null, mass=1)
     );
 
     threeObj.userData.ammoBody = body;
-    scene.add(threeObj);
 
     if (mass > 0 || true)
     {
@@ -235,6 +287,7 @@ function addBody(threeObj, ammoShape, pos=null, quat=null, mass=1)
     
     rigidBodyObjs.push(threeObj);
     ammoWorld.addRigidBody(body);
+    return body;
 }
 
 stage.onmousemove = function(e)
