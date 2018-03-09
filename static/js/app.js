ede14a644050da2509344b9b0f0c79b211fa7eb7
minujeong <minu.hanwool@gmail.com>
2018-03-09 08:15:34 +0800

    add test three scene

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,44 @@
 
-anime({
-    targets: "#main .el",
-    translateX: 100,
-});
+let W = stage.clientWidth;
+let H = stage.clientHeight;
+let VIEW_ANGLE = 45;
+let ASPECT_RATIO = W / H;
+let NEAR = 0.1;
+let FAR = 10000;
 
-scene = new THREE.Scene();
-console.log(scene);
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera()
+{
+    VIEW_ANGLE, ASPECT_RATIO, NEAR, FAR
+};
+camera.position.y = 15;
+camera.position.z = 30;
+camera.rotation.x = -0.5;
+scene.add(camera);
 
-var a = new THREE.Camera();
-console.log(a);
+let geom = new THREE.BoxGeometry(12, 12, 12);
+let material = new THREE.MeshPhongMaterial({ color: 0xffff00 });
+let entity = new THREE.Mesh(geom, material);
+scene.add(entity);
+
+let sunLight = new THREE.DirectionalLight(0xccddbb);
+sunLight.position.x = 0;
+sunLight.position.y = 5;
+sunLight.position.z = -5;
+scene.add(sunLight);
+
+let ambientLight = new THREE.AmbientLight(0x301030);
+scene.add(ambientLight);
+
+renderer.setSize(W, H);
+stage.appendChild(renderer.domElement);
+
+function update()
+{
+    renderer.render(scene, camera);
+    requestAnimationFrame(update);
+
+    entity.rotateY(0.025);
+}
+requestAnimationFrame(update);
