547c4078be05209a393112714cd302b6415a15d8
Minwoo Jung <minwoo.jung@ubisoft.com>
2018-03-09 14:43:40 +0800

    add text mesh testing

--- static/js/app.js
+++ static/js/app.js
@@ -1,44 +1,97 @@
 
-let W = stage.clientWidth;
-let H = stage.clientHeight;
-let VIEW_ANGLE = 45;
-let ASPECT_RATIO = W / H;
-let NEAR = 0.1;
-let FAR = 10000;
+const W = stage.clientWidth;
+const H = stage.clientHeight;
+let renderer = null;
+let scene = null;
+let camera = null;
+let entity = null;
+let light = null;
 
-const renderer = new THREE.WebGLRenderer({ alpha: true });
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera()
+const defaultVertexShader = `
+
+void main()
 {
-    VIEW_ANGLE, ASPECT_RATIO, NEAR, FAR
-};
-camera.position.y = 15;
-camera.position.z = 30;
-camera.rotation.x = -0.5;
-scene.add(camera);
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}
+`;
 
-let geom = new THREE.BoxGeometry(12, 12, 12);
-let material = new THREE.MeshPhongMaterial({ color: 0xffff00 });
-let entity = new THREE.Mesh(geom, material);
-scene.add(entity);
+const defaultFragShader = `
+void main()
+{
+    gl_FragColor = vec4(1, 1, 1, 1);
+}
+`;
 
-let sunLight = new THREE.DirectionalLight(0xccddbb);
-sunLight.position.x = 0;
-sunLight.position.y = 5;
-sunLight.position.z = -5;
-scene.add(sunLight);
+function init()
+{
+    renderer = new THREE.WebGLRenderer({ alpha: true });
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(
+        55, W / H, 0.1, 10000
+    );
+    camera.position.y = 15;
+    camera.position.z = 30;
+    camera.rotation.x = -Math.atan2(camera.position.y, camera.position.z);
+    scene.add(camera);
 
-let ambientLight = new THREE.AmbientLight(0x301030);
-scene.add(ambientLight);
+    entity = new THREE.Object3D();
+    scene.add(entity);
 
-renderer.setSize(W, H);
-stage.appendChild(renderer.domElement);
+    let fontloader = new THREE.FontLoader();
+    fontloader.load("static/fonts/Ubuntu_Bold.json", function(font)
+        {
+            let geom = new THREE.TextGeometry(
+                "Abstract", {
+                    size: 2, height: 0.1, curveSegments: 5,
+                    font: font,
+                });
+            let material = new THREE.MeshStandardMaterial();
+            let subentity = new THREE.Mesh(geom, material);
+            subentity.position.x = -5;
+            entity.add(subentity);
+        });
+
+    light = new THREE.PointLight(0xffffff, 1.0);
+    light.position.y = 6;
+    light.position.z = 6;
+    scene.add(light);
+
+    let ambientLight = new THREE.AmbientLight(0x403080);
+    scene.add(ambientLight);
+
+    renderer.setSize(W, H);
+    stage.appendChild(renderer.domElement);
+}
 
 function update()
 {
     renderer.render(scene, camera);
-    requestAnimationFrame(update);
+    entity.rotateY(0.0125);
 
-    entity.rotateY(0.025);
+    requestAnimationFrame(update);
 }
+
+stage.onmousemove = function(e)
+{
+    light.position.x = -20 + (e.clientX / W) * 40;
+    light.position.y = 20 - (e.clientY / H) * 40;
+    light.position.z = -light.position.y;
+}
+
+stage.onmousedown = function(e)
+{
+    
+}
+
+stage.onmouseup = function(e)
+{
+    
+}
+
+stage.onmouseleave = function(e)
+{
+    stage.onmouseup(e);
+}
+
+init();
 requestAnimationFrame(update);
